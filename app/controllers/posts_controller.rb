70dc17887aa5e3f818dd975832504293b0c915c4
Adam Johnson <adamwjo@gmail.com>
2022-02-24 12:34:43 -0500

    new changes and setup

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,7 +9,7 @@ class PostsController < ApplicationController
         post = Post.find(params[:id])
 
         if post
-            render json: post, status: :ok
+            render json: post, status: :ok 
         else
             render json: {message: "Sorry cant find that bro"}, status: :not_found
         end
@@ -22,6 +22,35 @@ class PostsController < ApplicationController
         else
             render json: {errors: new_post.errors.full_messages}, status: :unprocessable_entity 
         end
+    end
+
+    def update
+        #get the param so we can find the entity we are trying to update
+        my_post = Post.find(params[:id]) #we'll try to find it
+        if my_post
+            #if we find it, update it
+            my_post.update(post_params)
+            render json: my_post
+        else
+            render json: {message: "cant find the post you're looking for"}, status: :not_found
+        end
+
+        #if we cant we'll let people know
+    end
+
+    def destroy
+        #get the param so we can find the entity we are trying to destory
+        my_post = Post.find(params[:id])
+        #we'll try to find it
+        if my_post
+            my_post.destroy
+            render json: {message: "DELETED"}
+        else
+            render json: {message: my_post.errors.full_messages}, status: :not_found
+        end
+        #if we find it, delete it, we'll let people know it's deleted
+
+        #if we cant we'll let people know
     end
 
     private
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :posts
 
-  get '/posts', to: "posts#index"
-  get '/posts/:id', to: "posts#show"
-  post '/posts', to: "posts#create"
+  # get '/posts', to: "posts#index"
+  # get '/posts/:id', to: "posts#show"
+  # post '/posts', to: "posts#create"
+  # patch '/posts/:id', to: "posts#update"
+
+  # get '/me' , to: "auth#getprofile"
 
 end
