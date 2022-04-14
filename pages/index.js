c1c0838f4513463e1434b14fca357d9237140597
gehrig_barnes <gehrigbarnes@gmail.com>
2022-04-14 16:14:14 -0500

    fixed file structure updated

--- pages/index.js
+++ pages/index.js
@@ -4,12 +4,14 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div>
-      <main >
-        <h1 >
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <h1 className={styles.title}>
           Hello World
         </h1>
       </main>
+
+     
     </div>
   )
 }
