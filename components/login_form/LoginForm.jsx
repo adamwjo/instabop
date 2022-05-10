4458dc36d4b24589ef4088b6f99e6010a4856487
gehrig_barnes <gehrigbarnes@gmail.com>
2022-05-10 12:51:53 -0500

    created a route for the login and a basic login form

--- components/login_form/LoginForm.jsx
+++ components/login_form/LoginForm.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
+class LoginForm extends React.Component {
+  state = {
+    email: '',
+    password: ''
+  }
 
-export default function LoginForm() {
-  return (
-    <div>LoginForm</div>
-  )
+  handleChange = e => {
+    this.setState({[e.target.name]: e.target.value});
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(this.state)
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <div><input type="email" name="email" placehoder="email" onChange={this.handleChange}/></div>
+        <div><input type="password" name="password" placeholder="password" onChange={this.handleChange}/></div>
+        <button type="submit">Login</button>
+      </form>
+    )
+  }
 }
+
+export default LoginForm
